0428a7a0c0b5ed4ca0bf9e9b5539e8bb48bf2eb9
Jae Aboleth <jae.rockets@gmail.com>
2022-01-26 20:26:39 -0800

    server shenangingae

--- server.js
+++ server.js
@@ -2,13 +2,30 @@ const http = require('http');
 const fs = require('fs')
 
 const server = http.createServer(function (request, response) {
-    request.on('data', (chunk) => {
-        console.log(chunk.toString())
-        console.log(request.socket.remoteAddress)
-      });
-    response.writeHead(200, { 'Access-Control-Allow-Headers': '*', 'Access-Control-Allow-Origin': '*',  'Content-Type': 'application/json' });
-    response.end()
-    })
+    var mousePositions = {}
+    if (request.method == 'OPTIONS') {
+        response.writeHead(200, { 'Access-Control-Allow-Headers': '*', 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' });
+        response.end()
+    }
+    if (request.method == 'POST') {
+        request.on('data', (chunk) => {
+            mousePositions[request.socket.remoteAddress] = JSON.parse(chunk.toString())
+        });
+        response.writeHead(200, { 'Access-Control-Allow-Headers': '*', 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' });
+        response.end()
+    }
+    if (request.method == 'GET') {
+        ipsToSend = []
+        console.log(ipsToSend)
+        for (var ip in mousePositions) {
+            if (ip != request.socket.remoteAddress) {
+                ipsToSend.push(mousePositions[ip])
+            }
+        }
+        response.writeHead(200, { 'Access-Control-Allow-Headers': '*', 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(ipsToSend))
+    }
+})
 
 const port = 3200
 const host = '2620:9b::1901:900a'
