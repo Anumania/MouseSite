3167d34979763870c45d05dfffebd8cdbc8dd552
Jae Aboleth <jae.rockets@gmail.com>
2022-01-26 16:49:15 -0800

    pusha t

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const http = require('http');
 const fs = require('fs')
 
 const server = http.createServer(function (request, response) {
-    
+    request.on('data', (chunk) => {
+        console.log(chunk.toString())
+        console.log(request.socket.remoteAddress)
+      });
+    response.writeHead(200, { 'Access-Control-Allow-Headers': '*', 'Access-Control-Allow-Origin': '*',  'Content-Type': 'application/json' });
+    response.end()
     })
 
 const port = 3200
-const host = '127.0.0.1'
+const host = '2620:9b::1901:900a'
 server.listen(port, host)
 console.log(`Listening at http://${host}:${port}`)
